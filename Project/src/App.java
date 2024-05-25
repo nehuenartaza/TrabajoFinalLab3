84b03a215b325327d782fb0058660397c50a804e
FedeAristegui <fedearistegui21@gmail.com>
2024-05-25 17:31:55 -0300

    test deletear branch

--- Project/src/App.java
+++ Project/src/App.java
@@ -2,10 +2,5 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("otra prueba");
-        xdd();
-    }
-    public static void xdd(){
-        System.out.println("test de push Fede");
-        System.out.println("Comprate un internet mejor amigo.");
     }
 }
