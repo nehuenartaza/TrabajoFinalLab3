c94bfa858e9918872f452fbbb2ceb1e79c8868f3
FedeAristegui <fedearistegui21@gmail.com>
2024-05-25 17:06:19 -0300

    xdd
    test push fede

--- Project/src/App.java
+++ Project/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("otra prueba");
+        xdd();
+    }
+    public static void xdd(){
+        System.out.println("test de push Fede");
+        System.out.println("Comprate un internet mejor amigo.");
     }
 }
